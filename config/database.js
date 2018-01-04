e442a6f7e30789619c2bf5949fbefd87e08f319c
Tomas Pizarro <tomas@bitnami.com>
2018-01-04 12:22:44 +0000

    Set /mnt as public folder

--- config/database.js
+++ config/database.js
@@ -3,19 +3,11 @@ const databasePort = process.env.DATABASE_PORT;
 const databaseUser = process.env.DATABASE_USER;
 const databasePassword = process.env.DATABASE_PASSWORD;
 const databaseName = process.env.DATABASE_NAME;
-console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-console.log(`      databaseHost is ${databaseHost}`);
-console.log(`      databasePort is ${databasePort}`);
-console.log(`      databaseUser is ${databaseUser}`);
-console.log(`      databasePassword is ${databasePassword}`);
-console.log(`      databaseName is ${databaseName}`);
-console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-console.log(`remoteUrl is => mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}`);
-console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
 module.exports = {
+  // Examples of valid connection strings.
   //remoteUrl : 'mongodb://todo:bitnami@mongodb-primary:27017/todo',
-  remoteUrl : `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?ssl=true`,
   //remoteUrl : 'mongodb://0eb6bfe9-0ee0-4-231-b9ee:PpqNdxdyyys5nnQNA6SmPatk4NGkPlkLpUeqYz33ikQKTNDy4cma42500PCpt8S0GF9qm0hzv0R0FKglK3v03g==@0eb6bfe9-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true',
+  remoteUrl : `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?ssl=true`,
   localUrl: 'mongodb://localhost/meanstacktutorials'
 };
