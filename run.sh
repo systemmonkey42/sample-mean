452bf019bf3f73f65f8f491548f4e76b3678e3e6
Joseda Rios <josriolop@gmail.com>
2018-01-09 11:50:07 +0100

    load env file

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 
-# Move to application folder first
-cd ${APP_FOLDER}
+echo "==> Printing env"
+env
+
+. /opt/bitnami/.env
 
 echo "==> Printing env"
 env
+
+# Move to application folder first
+cd ${APP_FOLDER}
 
 case "$1" in
   start)
@@ -25,6 +30,7 @@ case "$1" in
       npm install
       touch .initialized
 
+      # Use env vars
       sed -i "s/process\.env\.DATABASE_PASSWORD/\"${APP_PASSWORD}\"/g" config/database.js
     else
       echo "==> Aplication already initialized. Skipping ..."
