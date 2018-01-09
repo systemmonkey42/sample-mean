9c06373ee75b199e951d62c872aab2312425ff0c
Joseda Rios <josriolop@gmail.com>
2018-01-09 16:04:05 +0100

    use pm2 to stop-start-restart

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 # Move to application folder first
 cd ${APP_FOLDER}
 
+# TODOC: Bash + npm style
 start () {
   npm start &
   ps x -o  "%p %r" | grep $! | awk '{print $2}' > ${APP_FOLDER}/app.pid
@@ -13,7 +14,7 @@ start () {
 
 stop () {
   pid=$(cat ${APP_FOLDER}/app.pid)
-  if [ -n "${pid}" ];
+  if [ -n "${pid}" ]; then
     kill -SIGTERM -- -${pid}
     rm ${APP_FOLDER}/app.pid
   fi
@@ -21,26 +22,26 @@ stop () {
 
 case "$1" in
   start)
-    start
+    pm2 start server.js --name node-todo
     exit $?
     ;;
   stop)
-    stop
+    pm2 stop node-todo
     exit $?
     ;;
   restart|force-reload|reload)
-    stop
-    start
+    pm2 restart node-todo
     exit $?
     ;;
   init)
     if [[ ! -f .initialized ]]; then
-      echo "==> Aplication not initialized. Initializing now ..."
+      echo "==> Aplication not initialized. Initializing now..."
 
       # Install node modules
       npm install
 
       # Move static files to mount point
+      rm -rf ${DATA_FOLDER}
       mkdir -p ${DATA_FOLDER}
       mv public ${DATA_FOLDER}
       ln -sf ${DATA_FOLDER}/public public
@@ -48,6 +49,6 @@ case "$1" in
       # Touch semaphore
       touch .initialized
     else
-      echo "==> Aplication already initialized. Skipping ..."
+      echo "==> Aplication already initialized. Skipping..."
     fi
 esac
