c070e991eed5faa9831034b0e3c473e7cb697143
Joseda Rios <josriolop@gmail.com>
2018-01-09 16:56:32 +0100

    fix typo

--- config/database.js
+++ config/database.js
@@ -9,6 +9,6 @@ module.exports = {
   // Examples of valid connection strings.
   //remoteUrl : 'mongodb://todo:bitnami@mongodb-primary:27017/todo',
   //remoteUrl : 'mongodb://0eb6bfe9-0ee0-4-231-b9ee:PpqNdxdyyys5nnQNA6SmPatk4NGkPlkLpUeqYz33ikQKTNDy4cma42500PCpt8S0GF9qm0hzv0R0FKglK3v03g==@0eb6bfe9-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true',
-  remoteUrl : `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?{databaseConnectionOpts}`,
+  remoteUrl : `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?${databaseConnectionOpts}`,
   localUrl: 'mongodb://localhost/meanstacktutorials'
 };
