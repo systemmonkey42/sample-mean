e4f2088fd4551e443b41531327e0842eca8370fc
Joseda Rios <jotadrilo@users.noreply.github.com>
2018-01-24 18:34:50 +0100

    decouple data and app initialization (#5)
    * decouple data and app initialization
    * fix pm2 uid and gid spawned processes

--- run.sh
+++ run.sh
@@ -22,46 +22,54 @@ stop () {
 
 case "$1" in
   start)
-    su - bitnami -c "pm2 start ${APP_FOLDER}/server.js --name node-todo"
+    pm2 start ${APP_FOLDER}/server.js --name node-todo --uid bitnami --gid bitnami
     exit $?
     ;;
   stop)
-    su - bitnami -c "pm2 stop node-todo"
+    pm2 stop node-todo --uid bitnami --gid bitnami
     exit $?
     ;;
   restart|force-reload|reload)
     # `--update-env` will allow to load environment variables from /bitnami/.env
     # in case they changed
-    su - bitnami -c "pm2 restart node-todo --update-env"
+    pm2 restart node-todo --update-env --uid bitnami --gid bitnami
     exit $?
     ;;
   init)
     # Sleep randomly between 1 and 10 seconds to avoid race conditions
     sleep $(($RANDOM % 10 + 1))
+
+    # Data initialization
     if [[ ! -f ${DATA_FOLDER}/.initialized ]]; then
-      echo "==> Aplication not initialized. Initializing now..."
+      echo "==> Data not initialized. Initializing now..."
       # Create data folder
       mkdir -p ${DATA_FOLDER}
       # Touch semaphore
-      su - bitnami -c "touch ${DATA_FOLDER}/.initialized"
+      touch ${DATA_FOLDER}/.initialized
 
       # Move static files to mount point
       mv public ${DATA_FOLDER}
       ln -sf ${DATA_FOLDER}/public ${APP_FOLDER}/public
 
       # Fix permissions
-      chown -R bitnami:bitnami ${APP_FOLDER}
       chown -R bitnami:bitnami ${DATA_FOLDER}
-      chown root:root ${APP_FOLDER}/run.sh
-
-      # Install dependencies
-      su - bitnami -c "cd ${APP_FOLDER}; npm install"
+      chmod 600 ${DATA_FOLDER}/.initialized
     else
-      echo "==> Aplication already initialized. Skipping..."
+      echo "==> Data already initialized. Skipping..."
       # Link static files
       rm -rf ${APP_FOLDER}/public
       ln -sf ${DATA_FOLDER}/public ${APP_FOLDER}/public
     fi
+
+    # Application initialization
+    echo "==> Initializing application..."
+
+    # Fix permissions
+    chown -R bitnami:bitnami ${APP_FOLDER}
+    chown root:root ${APP_FOLDER}/run.sh
+
+    # Install dependencies
+    su - bitnami -c "cd ${APP_FOLDER}; npm install"
     exit 0
     ;;
   *)
