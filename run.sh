26f27f44adf68b5532f0d53ac44d0144065e289e
Joseda Rios <jotadrilo@users.noreply.github.com>
2018-01-24 17:49:01 +0100

    fix run.sh su commands (#4)

--- run.sh
+++ run.sh
@@ -6,9 +6,6 @@
 # Load balancer will redirect HTTP (port 80) traffic to port 8080
 export PORT=8080
 
-# Move to application folder first
-cd ${APP_FOLDER}
-
 # TODOC: Bash + npm style
 start () {
   npm start &
@@ -25,41 +22,45 @@ stop () {
 
 case "$1" in
   start)
-    su bitnami pm2 start server.js --name node-todo
+    su - bitnami -c "pm2 start ${APP_FOLDER}/server.js --name node-todo"
     exit $?
     ;;
   stop)
-    su bitnami pm2 stop node-todo
+    su - bitnami -c "pm2 stop node-todo"
     exit $?
     ;;
   restart|force-reload|reload)
     # `--update-env` will allow to load environment variables from /bitnami/.env
     # in case they changed
-    su bitnami pm2 restart node-todo --update-env
+    su - bitnami -c "pm2 restart node-todo --update-env"
     exit $?
     ;;
   init)
+    # Sleep randomly between 1 and 10 seconds to avoid race conditions
+    sleep $(($RANDOM % 10 + 1))
     if [[ ! -f ${DATA_FOLDER}/.initialized ]]; then
       echo "==> Aplication not initialized. Initializing now..."
+      # Create data folder
+      mkdir -p ${DATA_FOLDER}
+      # Touch semaphore
+      su - bitnami -c "touch ${DATA_FOLDER}/.initialized"
 
       # Move static files to mount point
-      rm -rf ${DATA_FOLDER}
-      mkdir -p ${DATA_FOLDER}
       mv public ${DATA_FOLDER}
-      ln -sf ${DATA_FOLDER}/public public
+      ln -sf ${DATA_FOLDER}/public ${APP_FOLDER}/public
 
       # Fix permissions
       chown -R bitnami:bitnami ${APP_FOLDER}
       chown -R bitnami:bitnami ${DATA_FOLDER}
       chown root:root ${APP_FOLDER}/run.sh
 
-      # Install node modules
-      su bitnami npm install
-
-      # Touch semaphore
-      su bitnami touch ${DATA_FOLDER}/.initialized
+      # Install dependencies
+      su - bitnami -c "cd ${APP_FOLDER}; npm install"
     else
       echo "==> Aplication already initialized. Skipping..."
+      # Link static files
+      rm -rf ${APP_FOLDER}/public
+      ln -sf ${DATA_FOLDER}/public ${APP_FOLDER}/public
     fi
     exit 0
     ;;
