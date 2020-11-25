1f7c1877ae97c41441dde8e1e7055684e7036479
David le Blanc <david-le-blanc@users.noreply.github.com>
2020-11-25 13:49:05 +1100

    Stupid hack

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ var paths = require('../config/paths');
 
 function getTodos(res) {
     Todo.find(function (err, todos) {
-
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err) {
             res.send(err);
@@ -11,10 +10,9 @@ function getTodos(res) {
 
         res.json(todos); // return all todos in JSON format
     });
-};
+}
 
 module.exports = function (app) {
-
     // api ---------------------------------------------------------------------
     // get all todos
     app.get('/api/todos', function (req, res) {
@@ -22,37 +20,43 @@ module.exports = function (app) {
         getTodos(res);
     });
 
+    app.get('/api/env', function (req, res) {
+        res.send(process.env);
+    });
+
     // create todo and send back all todos after creation
     app.post('/api/todos', function (req, res) {
-
         // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text: req.body.text,
-            done: false
-        }, function (err, todo) {
-            if (err)
-                res.send(err);
+        Todo.create(
+            {
+                text: req.body.text,
+                done: false,
+            },
+            function (err, todo) {
+                if (err) res.send(err);
 
-            // get and return all the todos after you create another
-            getTodos(res);
-        });
-
+                // get and return all the todos after you create another
+                getTodos(res);
+            }
+        );
     });
 
     // delete a todo
     app.delete('/api/todos/:todo_id', function (req, res) {
-        Todo.remove({
-            _id: req.params.todo_id
-        }, function (err, todo) {
-            if (err)
-                res.send(err);
+        Todo.remove(
+            {
+                _id: req.params.todo_id,
+            },
+            function (err, todo) {
+                if (err) res.send(err);
 
-            getTodos(res);
-        });
+                getTodos(res);
+            }
+        );
     });
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-      res.sendFile(paths.staticFiles); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(paths.staticFiles); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
